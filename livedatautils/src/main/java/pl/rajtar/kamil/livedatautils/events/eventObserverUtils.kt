2f680c9d482b92e00c94ce3987557aa04e4cae36
Kamil Rajtar <Kamil@rajtar.pl>
2019-11-01 09:18:33 +0100

    1.1.2 added observeEvents

--- livedatautils/src/main/java/pl/rajtar/kamil/livedatautils/events/eventObserverUtils.kt
+++ livedatautils/src/main/java/pl/rajtar/kamil/livedatautils/events/eventObserverUtils.kt
@@ -1,6 +1,11 @@
 package pl.rajtar.kamil.livedatautils.events
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 
 fun <T : Any> Observer<T>.toEventObserver() =
     EventObserverWrapper(this)
+
+fun <T : Any> LiveData<Event<T>>.observeEvents(lifecycleOwner:LifecycleOwner,observer: Observer<T>) =
+    observe(lifecycleOwner,observer.toEventObserver())
